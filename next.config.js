a5cacb552d3066c58df3570dd1d0516498f07d4b
acambece123 <jacambece0987@gmail.com>
2025-10-07 12:26:57 -0400

    update next.config.js for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  output: 'export',                // 👈 tells Next.js to create static HTML
+  basePath: '/debt-buyer-lawsuits-ma', // 👈 match your repo name
+  images: {
+    unoptimized: true,             // 👈 GitHub Pages doesn’t handle Next.js images
+  },
 }
 
 module.exports = nextConfig
